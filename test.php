5581f3025dd958e1f37875c9c96e6973b393e9a2
etokana1 <namin@bknix.co.th>
2015-09-15 13:07:33 +0700

    s

---
+++ test.php
@@ -0,0 +1,16 @@
+<?
+$username="root";
+$server="vpn0";
+$command="ifconfig";
+#$str = "/var/www/bknix/gui/sudo /var/www/bknix/gui/ssh -o StrictHostKeyChecking=no ".$username."@".$server." ".$command;
+#$out=shell_exec('/var/www/bknix/gui/ssh -o StrictHostKeyChecking=no root@vpn0');
+#exec($str,$out);
+#$out=exec('/var/www/bknix/gui/sudo ifconfig');
+#echo '<pre>'; print_r($out); echo '</pre>';
+#echo $out;
+
+#$connection = ssh2_connect('vpn0.bknix.co.th', 22);
+#ssh2_auth_password($connection, 'root', '');
+#$stream = ssh2_exec($connection, '/usr/local/bin/php -i');
+#echo $stream;
+?>
